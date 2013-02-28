ed38e233baffb9b3dfe6560df80634a4a22ca64b
Nathaniel Griswold <nategriswold@gmail.com>
2013-02-28 12:42:30 -0800

    Restore blocking operation category

---
+++ SocializeAPIClient/NSOperationQueue+BlockingOperation.h
@@ -0,0 +1,15 @@
+//
+//  NSOperationQueue+BlockingOperation.h
+//  SocializeAPIClient
+//
+//  Created by Nate Griswold on 2/25/13.
+//  Copyright (c) 2013 Socialize. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSOperationQueue (BlockingOperation)
+
+- (void)addBlockingOperation:(NSOperation*)operation dontBlock:(BOOL(^)(NSOperation *otherOperation))dontBlock;
+
+@end
