2345860569d98e857c041522ce4c43f2b1a73735
Nate Griswold <nategriswold@gmail.com>
2013-03-01 16:14:05 -0800

    Test completion category

---
+++ SocializeAPIClient Tests/NSOperation+AdditionalCompletionTests.h
@@ -0,0 +1,13 @@
+//
+//  NSOperation+AdditionalCompletionTests.h
+//  SocializeAPIClient
+//
+//  Created by Nate Griswold on 3/1/13.
+//  Copyright (c) 2013 Socialize. All rights reserved.
+//
+
+#import "SZTestCase.h"
+
+@interface NSOperation_AdditionalCompletionTests : SZTestCase
+
+@end
---
+++ SocializeAPIClient Tests/NSOperation+AdditionalCompletionTests.m
@@ -0,0 +1,77 @@
+//
+//  NSOperation+AdditionalCompletionTests.m
+//  SocializeAPIClient
+//
+//  Created by Nate Griswold on 3/1/13.
+//  Copyright (c) 2013 Socialize. All rights reserved.
+//
+
+#import "NSOperation+AdditionalCompletionTests.h"
+#import "NSOperation+AdditionalCompletion.h"
+
+@interface TestOperation : NSOperation
+@end
+
+@implementation TestOperation
+
+- (void)main {
+    NSLog(@"Running op %@", self);
+    
+}
+
+@end
+
+@interface NSOperation_AdditionalCompletionTests ()
+@property (nonatomic, strong) NSOperationQueue *operationQueue;
+@end
+
+@implementation NSOperation_AdditionalCompletionTests
+
+- (NSOperationQueue *)operationQueue {
+    if (_operationQueue == nil) {
+        _operationQueue = [[NSOperationQueue alloc] init];
+    }
+    return _operationQueue;
+}
+
+- (void)testAddSingleOperation {
+    TestOperation *operation = [[TestOperation alloc] init];
+    
+    __block BOOL one = NO, two = NO;
+    [operation addCompletionBlock:^{
+        one = YES;
+    }];
+
+    [operation addCompletionBlock:^{
+        two = YES;
+    }];
+
+    
+    [self.operationQueue addOperation:operation];
+    [self.operationQueue waitUntilAllOperationsAreFinished];
+    
+    GHAssertTrue(one, @"One not called");
+    GHAssertTrue(two, @"Two not called");
+}
+
+- (void)testAddMultipleOperations {
+    
+    TestOperation *o1 = [[TestOperation alloc] init];
+    __block BOOL one = NO, two = NO;
+    [o1 addCompletionBlock:^{
+        one = YES;
+    }];
+    
+    TestOperation *o2 = [[TestOperation alloc] init];
+//    __block BOOL two = NO;
+    [o2 addCompletionBlock:^{
+        two = YES;
+    }];
+    
+    [self.operationQueue addOperations:@[ o1, o2 ] waitUntilFinished:NO];
+    [self.operationQueue waitUntilAllOperationsAreFinished];
+    GHAssertTrue(one, @"One not called");
+    GHAssertTrue(two, @"Two not called");
+}
+
+@end
