24386a29d51c773b79f3cc342f366d2237c39592
Nate Griswold <nategriswold@gmail.com>
2013-03-01 16:50:57 -0800

    Add base operation tests

---
+++ SocializeAPIClient Tests/SZOperationTests.m
@@ -0,0 +1,54 @@
+//
+//  SZOperationTests.m
+//  SocializeAPIClient
+//
+//  Created by Nate Griswold on 3/1/13.
+//  Copyright (c) 2013 Socialize. All rights reserved.
+//
+
+#import "SZOperationTests.h"
+#import "SZOperation.h"
+
+@interface SZTestOperation : SZOperation
+@property (nonatomic, assign) BOOL shouldFail;
+@end
+
+@implementation SZTestOperation
+
+- (void)main {
+    NSLog(@"Running op");
+    self.didFail = self.shouldFail;
+}
+
+@end
+
+@interface SZOperationTests ()
+@property (nonatomic, strong) NSOperationQueue *operationQueue;
+@end
+
+@implementation SZOperationTests
+
+- (NSOperationQueue *)operationQueue {
+    if (_operationQueue == nil) {
+        _operationQueue = [[NSOperationQueue alloc] init];
+    }
+    return _operationQueue;
+}
+
+- (void)testFailingDependency {
+    
+    SZTestOperation *op1 = [[SZTestOperation alloc] init];
+    op1.shouldFail = YES;
+    
+    SZTestOperation *op2 = [[SZTestOperation alloc] init];
+    [op2 addDependency:op1];
+    
+    [self.operationQueue addOperations:@[ op1, op2 ] waitUntilFinished:NO];
+    [self.operationQueue waitUntilAllOperationsAreFinished];
+
+    NSError *error = [op2 failedDependenciesError];
+    NSArray *failedDependencies = [[error userInfo] objectForKey:SZErrorFailedDependenciesKey];
+    GHAssertTrue([failedDependencies containsObject:op1], @"Should have failed deps");
+}
+
+@end
